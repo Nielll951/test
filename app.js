5f60f183f5124424bb33d67c9d6683e45c7c3a42
Kostya <rodina9180gaocheng@ukr.net>
2021-01-20 21:42:28 +0300

    added js to product-count, to buttons more-details and add-to-cart

--- app.js
+++ app.js
@@ -3,31 +3,34 @@ let productsCountEl = document.getElementById("products-count");// at first we s
 //console.log(productsCountEl);
 let addToCartButtons = document.querySelectorAll(".add-to-cart");// at first we should find the elements
 //console.log(addToCartButtons)
-//нельзя задать addEventListener для псевдомассива
+//можно задать addEventListener для одного эл., но не для неск.эл.(псевдомассива).
 for(let i = 0;i < addToCartButtons.length;i++) { // перебираем все кнопки add-to-cart циклом for
     addToCartButtons[i].addEventListener("click",function() {
-        productsCountEl.textContent = +productsCountEl.textContent + 1;
-    })
+        productsCountEl.textContent = +productsCountEl.textContent + 1; 
+   })
 }
 
 //modal
+
 let modal = document.querySelector(".modal");
 let moreDetailsBtns = document.querySelectorAll(".more-details");
 let closeBtn = document.querySelector(".btn-close");
 
-//forEach
-moreDetailsBtns.forEach((btn) => {
-    btn.addEventListener("click",function() {
-        modal.classList.add("show");
-        modal.classList.remove("hide");
 
-    })
+//forEach
+moreDetailsBtns.forEach((btn) => { //на пер. moreDetailsBtns вешаем ф.forEach, где btn- текущий эл.
+    btn.addEventListener("click",openModal) 
 })
 
-function closeModal() {
-    modal.classList.add("hide");
-    modal.classList.remove("show");
+function openModal() {
+    modal.classList.add("show");// свойство.метод classList.add добавляет класс.
+    modal.classList.remove("hide");// свойство.метод classList.remove удаляет класс
 }
+
+ function closeModal() {
+     modal.classList.add("hide");// свойство.метод classList.add добавляет класс.
+     modal.classList.remove("show");// свойство.метод classList.remove удаляет класс
+ }
 
 closeBtn.addEventListener("click",closeModal)
 
@@ -36,4 +39,39 @@ modal.addEventListener("click",function(e) {
         closeModal()
     }
 })
-    
+
+// console.log(window.pageYOffset) // сколько проскролили
+// console.log(document.body.scrollHeight) // высота странички
+
+function showModalByScroll() {
+        window.removeEventListener("scroll",showModalByScroll)
+    }
+
+
+function showModalByScroll() {
+    if(window.pageYOffset > document.body.scrollHeight/2) {
+        openModal();
+        window.removeEventListener("scroll",showModalByScroll)// на событии scroll удалить ф. showModalByScroll.
+    }
+}
+
+ window.addEventListener("scroll",showModalByScroll);
+   
+// change like button state
+
+ let likeButtons = document.querySelectorAll('.heart');
+
+ likeButtons.forEach((btn) => {
+     btn.addEventListener("click",function(e) {
+         this.classList.toggle("liked")
+        //  console.log(e.target) // можно прописать так.
+        // if(this.classList.contains("liked")) {// this указывает на эл., по которому мы кликнули.
+        //     this.classList.remove("liked")
+        // } else {
+        //     this.classList.add("liked")
+        // }
+    })
+ })
+
+
+
